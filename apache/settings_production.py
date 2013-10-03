cfa262ad74ae50672d4430c4196d02f514f6d82b
joelfischer <jef@cs.nott.ac.uk>
2013-10-03 16:26:44 +0100

    udpated media setting

--- apache/settings_production.py
+++ apache/settings_production.py
@@ -47,7 +47,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/jef/homemade/static/'#'/home/jef/foody'#os.path.join(ROOT_PATH, 'media')
+MEDIA_ROOT = '/home/jef/homemade/media/'#'/home/jef/foody'#os.path.join(ROOT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
