5748871df8dafd59a2e0cbf23fe6c5db896eeaae
joelfischer <jef@cs.nott.ac.uk>
2013-10-03 15:39:06 +0100

    mysql settings

--- settings.py
+++ settings.py
@@ -16,10 +16,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/jef/Dev/homemadeshop/db/sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'homemadeshop_db',                       # Or path to database file if using sqlite3.
+        'USER': 'homemade',                      # Not used with sqlite3.
+        'PASSWORD': 'h0mem4de!',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
