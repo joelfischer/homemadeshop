e89e818a7c51ea0fd526088c14328c76ee6f447f
joelfischer <jef@cs.nott.ac.uk>
2013-10-03 20:37:24 +0100

    uncommented

--- apache/urls_production.py
+++ apache/urls_production.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'shop.views.site', {'site': 'START'}),
     url(r'^home/$', 'shop.views.index'),
     url(r'^hockley/$', 'shop.views.index'),
     url(r'^menu/$', 'shop.views.site', {'site': 'MENU'}),
@@ -43,13 +44,12 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 #    url(r'^shop/admin/', include(admin.site.urls)),
-    url(r'^$', 'shop.views.site', {'site': 'START'}),
 #    url(r'^', include('cms.urls')),
 )
 
-if settings.DEBUG:
-    urlpatterns = patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'', include('django.contrib.staticfiles.urls')),
-) + urlpatterns
+#if settings.DEBUG:
+#    urlpatterns = patterns('',
+#    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+#        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+#    url(r'', include('django.contrib.staticfiles.urls')),
+#) + urlpatterns
