856f6c4be8c13d71da096a5e8df33ad0ba139fa5
joelfischer <jef@cs.nott.ac.uk>
2013-10-04 13:44:24 +0100

    media file changes

--- settings.py
+++ settings.py
@@ -90,12 +90,7 @@ else:
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-if HOSTING == 'DEV':
-    MEDIA_URL = '/media/'
-else:
-    MEDIA_URL = os.path.join(MEDIA_ROOT, 'media');
-    
-
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
