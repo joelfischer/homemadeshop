2cf40c72812b1602b070ab5b74a5f06931197f3e
joelfischer <jef@cs.nott.ac.uk>
2016-11-12 19:30:27 +0000

    email changed

--- email_settings.py
+++ email_settings.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'joelefischer@gmail.com' 
-EMAIL_HOST_PASSWORD = 'siiel_33'
+EMAIL_HOST_USER = 'gmail address' 
+EMAIL_HOST_PASSWORD = 'password'
